b91e9be57913e45d24478f6f03a4d4847591d923
Илон <vlasovvdm@gmail.com>
2023-10-02 21:40:31 +0400

    Я не смог выполнить 2 часть 8 задачи

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args) {
+        var x=640;
+        var y=8;
+        var z=(x/y)+94;
+        var v=640/(x/y)+94;
+        System.out.println(z=v);
+        }}
+
+
+
